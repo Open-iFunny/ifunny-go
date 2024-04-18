20d46fe2d1188975cc8306b8c772c51eecaa5f8e
eva <mail@gastrodon.io>
2024-04-18 12:19:50 -0400

    Add more fields to user struct

--- user.go
+++ user.go
@@ -15,11 +15,25 @@ type User struct {
 	Nick  string `json:"nick"`
 	About string `json:"about"`
 
-	IsAvailableForChat bool `json:"is_available_for_chat"`
-	IsBanned           bool `json:"is_banned"`
-	IsDeleted          bool `json:"is_deleted"`
-	IsModerator        bool `json:"is_moderator"`
-	IsVerified         bool `json:"is_verified"`
+	IsAvailableForChat    bool `json:"is_available_for_chat"`
+	IsBanned              bool `json:"is_banned"`
+	IsDeleted             bool `json:"is_deleted"`
+	IsModerator           bool `json:"is_moderator"`
+	IsVerified            bool `json:"is_verified"`
+	IsInSubscribers       bool `json:"is_in_subscribers"`
+	IsInSubscriptions     bool `json:"is_in_subscriptions"`
+	IsSubscribedToUpdates bool `json:"is_subscribed_to_updates"`
+	IsPrivate             bool `json:"is_private"`
+
+	Num struct {
+		Subscriptions int `json:"subscriptions"`
+		Subscribers   int `json:"subscribers"`
+		TotalPosts    int `json:"total_posts"`
+		Created       int `json:"created"`
+		Featured      int `json:"featured"`
+		TotalSmiles   int `json:"total_smiles"`
+		Achievements  int `json:"achievements"`
+	} `json:"num"`
 }
 
 func (client *Client) GetUser(desc compose.Request) (*User, error) {
