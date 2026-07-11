24914647968cf493d3f4fb715a8e9f0e8ef90ba5
Eva Harris <mail@gastrodon.io>
2026-07-10 22:35:39 -0400

    Extend Authored interface with AuthorNick
    Adds AuthorNick() to complement AuthorID() on Comment, Content, and
    ChatEvent, since each resource carries the author's nick alongside their ID.

--- authored.go
+++ authored.go
@@ -3,19 +3,23 @@ package ifunny
 // Authored represents an iFunny API resource that was created by a user.
 type Authored interface {
 	AuthorID() string
+	AuthorNick() string
 }
 
 // AuthorID returns the ID of the user who authored this comment.
-func (c *Comment) AuthorID() string {
-	return c.User.ID
-}
+func (c *Comment) AuthorID() string { return c.User.ID }
+
+// AuthorNick returns the nick of the user who authored this comment.
+func (c *Comment) AuthorNick() string { return c.User.Nick }
 
 // AuthorID returns the ID of the user who authored this content.
-func (c *Content) AuthorID() string {
-	return c.Creator.ID
-}
+func (c *Content) AuthorID() string { return c.Creator.ID }
+
+// AuthorNick returns the nick of the user who authored this content.
+func (c *Content) AuthorNick() string { return c.Creator.Nick }
 
 // AuthorID returns the ID of the user who authored this chat event.
-func (e *ChatEvent) AuthorID() string {
-	return e.User.ID
-}
+func (e *ChatEvent) AuthorID() string { return e.User.ID }
+
+// AuthorNick returns the nick of the user who authored this chat event.
+func (e *ChatEvent) AuthorNick() string { return e.User.Nick }
